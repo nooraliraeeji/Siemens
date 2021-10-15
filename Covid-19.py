870d4875941f3da2fc1268f2c9d416f10072d6e0
nooraliraeeji <37065864+nooraliraeeji@users.noreply.github.com>
2021-10-15 12:30:14 +0200

    Add files via upload

---
+++ Covid-19.py
@@ -0,0 +1,46 @@
+# Import the packages we need. 
+
+import pandas as pd
+import numpy as np
+import statistics
+import requests
+import io
+
+# Reading the downloaded content and turning it into a pandas dataframe
+data=pd.read_csv("https://raw.githubusercontent.com/nooraliraeeji/Siemens/main/input/covid_africa.csv")
+
+# creating 2 rank columns and passing the returned rank series
+data['Rank Total Cases/1 mil population']= data["Total Cases/1 mil population"].rank(ascending=False)
+data['Rank Deaths/1 mil population']= data['Deaths/1 mil population '].rank(ascending=False)
+
+data['severity index']=data['Rank Total Cases/1 mil population']*data['Rank Deaths/1 mil population']
+
+data=data.sort_values(by='severity index')
+
+result=data[['Country','severity index']]
+result.to_excel('Table1.xlsx',index=False)
+
+
+# ## Handling Missing value 
+
+data.dropna(inplace=True)
+
+#total recovered in the whole data set
+whole_total_recovered=data['Total Recovered'].sum()
+
+#median of total deaths from the whole data set.
+median_total_deaths=statistics.median(data['Total Deaths'])
+
+#standard deviation of the total tests from the whole data set.
+std_total_tests=statistics.stdev(data['Total Tests'])
+
+data2=data.head(3).copy()
+
+data2['Total recovered/whole_total_recovered']=data2['Total Recovered']/whole_total_recovered
+data2['Total Deaths/median_total_deaths']=data2['Total Deaths']/median_total_deaths
+data2['Total Tests/std_total_tests']=data2['Total Tests']/std_total_tests
+
+result2=data2[['Country','Total recovered/whole_total_recovered','Total Deaths/median_total_deaths','Total Tests/std_total_tests']]
+
+result2.to_excel('Table2.xlsx',index=False)
+
